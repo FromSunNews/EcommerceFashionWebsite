96c72e67fb07e8ec3d393c8cdc3039559741b163
Sinhz <sinhtranlk1@gmail.com>
2023-09-20 17:24:00 +0700

    update entire pages

---
+++ EcommerceFashionWebsite/Controllers/Blog-DetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceFashionWebsite.Controllers
+{
+    public class Blog_DetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
