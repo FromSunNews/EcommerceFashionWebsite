52ca91dc6bab364d632387715e57020fd36b40aa
FromSunNews <phuongtu590@gmail.com>
2023-10-24 13:44:18 +0700

    cartmodel

---
+++ EcommerceFashionWebsite/Models/CartModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceFashionWebsite.Models
+{
+    public class CartModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("ProductModel")]
+        public int ProductId { get; set; }
+        [ValidateNever]
+        public ProductModel ProductModel { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        [ValidateNever]
+        public UserModel UserModel { get; set; }
+
+    }
+}
